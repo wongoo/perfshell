1c2442a84e66565eb9c49b19e8d2c52d0772a5fd
wongoo <gelnyang@163.com>
2019-07-05 14:23:19 +0800

    sh exec

--- ab-test.sh
+++ ab-test.sh
@@ -13,7 +13,7 @@ vmstat_file="/tmp/ab-$concurrent-$loops-$process.vmstat"
 ab_file="/tmp/ab-$concurrent-$loops-$process.ab"
 rm -f ${ab_file}
 
-ssh -n ${host} "nohup /tmp/memstat.sh $process $memstat_file >/dev/null 2>&1 &"
+ssh -n ${host} "nohup sh /tmp/memstat.sh $process $memstat_file >/dev/null 2>&1 &"
 ssh -n ${host} "nohup vmstat 5 >$vmstat_file  &"
 sleep 5
 
@@ -30,8 +30,8 @@ echo "end ab concurrent: $concurrent, loops: $loops, process: $process"
 ssh ${host} "ps aux | grep -v grep | grep memstat  | cut -c 9-15 | xargs --no-run-if-empty kill -9"
 ssh ${host} "ps aux | grep -v grep | grep vmstat  | cut -c 9-15 | xargs --no-run-if-empty kill -9"
 
-ssh ${host} "/tmp/calcmem.sh $memstat_file" > /tmp/ab-mem.txt
-ssh ${host} "/tmp/calccpu.sh $vmstat_file" > /tmp/ab-cpu.txt
+ssh ${host} "sh /tmp/calcmem.sh $memstat_file" > /tmp/ab-mem.txt
+ssh ${host} "sh /tmp/calccpu.sh $vmstat_file" > /tmp/ab-cpu.txt
 
 mem=$(cat /tmp/ab-mem.txt)
 cpu=$(cat /tmp/ab-cpu.txt)
